d040ae4dd2208407e0b275df9bc8e182619c93a9
Benjamin-Guy <b.guy@uq.net.au>
2023-11-01 10:31:01 +1000

    Added code in test.py that can take screenshots of the Crossy Road game in a Bluestacks App Player window. The image is cropped to the region of the game correctly and saved as image files for viewing.

--- test.py
+++ test.py
@@ -1,1 +1,35 @@
-print("test")
+import pyautogui
+import pygetwindow as gw
+import time
+from PIL import Image
+
+def capture_bluestacks_screen(interval, duration):
+    # Find the BlueStacks window
+    bluestacks_window = gw.getWindowsWithTitle('BlueStacks App Player')[0]
+    
+    if bluestacks_window is not None:
+        start_time = time.time()
+
+        while (time.time() - start_time) < duration:
+            # Bring the BlueStacks window to the front
+            bluestacks_window.activate()
+
+            # Capture the screen region of the BlueStacks window
+            screenshot = pyautogui.screenshot(region=bluestacks_window.box)
+
+            # Determine the width and height of the screenshot
+            screenshot_width, screenshot_height = screenshot.size
+            crop_area = (0, 33, screenshot_width - 33, screenshot_height)
+
+            # Crop the image to the specified area
+            cropped_image = screenshot.crop(crop_area)
+
+            # Save or process the cropped screenshot
+            cropped_image.save(f"screenshot_{int(time.time())}.png")
+
+            # Wait for the next frame
+            time.sleep(interval)
+            break # This is for testing purposes.
+
+# Capture the screen of the BlueStacks window every 0.1 seconds for 0.5 seconds
+capture_bluestacks_screen(0.1, 0.5)
